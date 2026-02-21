f9aaff16a5955622f6030ec96432aba1ac9fc9d3
adriiana-dev <adrianateste52@gmail.com>
2026-02-21 11:35:57 -0300

    Simplify table creation statements in script_tabelas.sql
    ajuste

--- ddl/script_tabelas.sql
+++ ddl/script_tabelas.sql
@@ -1,58 +1,24 @@
-
-CREATE TABLE IF NOT EXISTS public.categorias
-(
-    codigo_unico integer NOT NULL DEFAULT nextval('categorias_codigo_unico_seq'::regclass),
-    nome_categoria character varying(80) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT categorias_pkey PRIMARY KEY (codigo_unico)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.categorias
-    OWNER to postgres;
+CREATE TABLE categorias (
+    codigo_unico INTEGER PRIMARY KEY,
+    nome_categoria VARCHAR(80) NOT NULL
+);
 
 
 
-CREATE TABLE IF NOT EXISTS public.metodos_pagamento
-(
-    codigo_pagamento integer NOT NULL,
-    formas_transacao character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT metodo_pagamento_pkey PRIMARY KEY (codigo_pagamento)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.metodos_pagamento
-    OWNER to postgres;
+CREATE TABLE metodos_pagamento (
+    codigo_pagamento INTEGER PRIMARY KEY,
+    formas_transacao VARCHAR(30) NOT NULL
+);
 
 
-CREATE TABLE IF NOT EXISTS public.funcionarios
-(
-    cpf character(11) COLLATE pg_catalog."default" NOT NULL,
-    nome_completo character varying(80) COLLATE pg_catalog."default" NOT NULL,
-    cargo character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    login_usuario character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT funcionarios_pkey PRIMARY KEY (cpf)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.funcionarios
-    OWNER to postgres;
+CREATE TABLE funcionarios (
+    cpf CHARACTER(11) PRIMARY KEY,
+    nome_completo VARCHAR(80) NOT NULL,
+    cargo VARCHAR(40) NOT NULL,
+    login_usuario VARCHAR(20) NOT NULL
+);
 
 
 
-CREATE TABLE IF NOT EXISTS public.produtos
-(
-    codigo_produto integer NOT NULL,
-    nome character varying(80) COLLATE pg_catalog."default" NOT NULL,
-    valor_unitario numeric(12,2),
-    estoque integer NOT NULL,
-    codigo_categorias integer NOT NULL,
-    CONSTRAINT produtos_pkey PRIMARY KEY (codigo_produto)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.produtos
-    OWNER to postgres;
+CREATE TABLE produtos (
+   
