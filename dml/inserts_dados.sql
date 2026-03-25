08ee8c7f0143ca44612d849222a999d2204027f7
adriiana-dev <adrianateste52@gmail.com>
2026-03-25 15:40:17 -0300

    correção dml

--- dml/inserts_dados.sql
+++ dml/inserts_dados.sql
@@ -1,37 +1,20 @@
 -- SCRIPT DE MANIPULAÇÃO DE DADOS DML
 -- Ordem de execução:Tabelas independentes para dependentes
 
-INSERT INTO categorias (id_categoria, nome_categoria) VALUES 
-(1, 'Smash Burgers'), (2, 'Hambúrgueres Artesanais'), (3, 'Porções e Entradas'), (4, 'Bebidas Naturais e Refri'), (5, 'Sobremesas e Shakes');
+-- Categorias
+INSERT INTO categorias (id, nome) VALUES (1, 'Smash Burgers'), (2, 'Hambúrgueres Artesanais'), (3, 'Porções e Entradas'), 
+(4, 'Bebidas Naturais e Refri'), (5, 'Sobremesas e Shakes');
 
-INSERT INTO metodos_pagamento (id_pagamento, formas_transacao) VALUES 
+-- Metodos_pagamento
+INSERT INTO metodos_pagamento (id, forma_transacao) VALUES 
 (1, 'PIX'), (2, 'Cartão de Crédito'), (3, 'Cartão de Débito'), (4, 'Dinheiro'), (5, 'Vale Refeição');
 
-INSERT INTO mesas (id_mesa, capacidade, status) VALUES 
-(1, 2, 'Livre'), (2, 4, 'Ocupada'), (3, 4, 'Livre'), (4, 6, 'Reservada'), (5, 2, 'Livre');
-
-INSERT INTO turnos (id_turno, nome_turno, horario_inicio, horario_fim) VALUES 
+-- Turnos
+INSERT INTO turnos (id, nome, inicio, fim) VALUES 
 (1, 'Almoço', '10:00', '16:00'), (2, 'Jantar', '16:00', '22:00'), (3, 'Madrugada', '22:00', '04:00');
 
-INSERT INTO entregadores (id_entregador, nome, telefone, placa_veiculo) VALUES 
-(1, 'Tiago Silva', '11977771111', 'RTL-8A99'), (2, 'Roberto Santos', '11977772222', 'QKW-2B34'), (3, 'Fernando Costa', '11977773333', 'PLM-9C12');
-
-INSERT INTO fornecedores (id_fornecedor, cnpj, nome_fantasia, telefone, email) VALUES 
-(1, '12345678000199', 'Boi Nobre', '1130001010', 'vendas@boinobre.com.br'), 
-(2, '98765432000188', 'Hortifruti Raiz', '1130002020', 'pedidos@hortiraiz.com.br'), 
-(3, '45612378000177', 'Gela Guela', '1130003030', 'contato@gelaguela.com.br');
-
-INSERT INTO ingredientes (id_ingrediente, nome, unidade_medida, estoque_atual) VALUES 
-(1, 'Pão Brioche', 'Unid', 150), (2, 'Blend Bovino', 'Unid', 200), (3, 'Queijo Cheddar', 'Fatia', 400), 
-(4, 'Alface', 'Maço', 15), (5, 'Tomate', 'Kg', 12), (6, 'Bacon', 'Kg', 18), 
-(7, 'Batata', 'Kg', 80), (8, 'Maionese Verde', 'L', 5);
-
-INSERT INTO promocoes (id_promocao, nome_promocao, desconto_percentual, data_inicio, data_fim) VALUES 
-(1, 'Terça do Smash', 50.00, '2026-03-01', '2026-12-31'), 
-(2, 'Combo Galera', 15.00, '2026-03-06', '2026-03-06'), 
-(3, 'Mês de Aniversário', 10.00, '2026-03-01', '2026-03-31');
-
-INSERT INTO funcionarios (id_funcionario, cpf, nome_completo, cargo, login_usuario) VALUES 
+-- Funcionarios
+INSERT INTO funcionarios (id, cpf, nome, cargo, login) VALUES 
 (1, '10101010101', 'Marina Oliveira', 'Gerente', 'marina.oli'), (2, '20202020202', 'Kleber Dias', 'Chapeiro', 'kleber.chapa'), 
 (3, '30303030303', 'Julia Mendes', 'Atendente', 'julia.cx'), (4, '40404040404', 'Carlos Silva', 'Atendente', 'carlos.at'), 
 (5, '50505050505', 'Beatriz Lima', 'Cozinha', 'bia.cozinha'), (6, '60606060606', 'Ricardo Santos', 'Chapeiro', 'ricardo.ch'),
@@ -48,7 +31,8 @@ INSERT INTO funcionarios (id_funcionario, cpf, nome_completo, cargo, login_usuar
 (27, '91919191919', 'Grazi Massafera', 'Atendente', 'grazi.m'), (28, '13131313131', 'Tony Ramos', 'Caixa', 'tony.r'), 
 (29, '24242424242', 'Gloria Pires', 'Cozinha', 'gloria.p'), (30, '35353535353', 'Lima Duarte', 'Atendente', 'lima.d');
 
-INSERT INTO clientes (id_cliente, cpf, nome_completo, telefone, endereco, data_cadastro) VALUES 
+-- Clientes
+INSERT INTO clientes (id, cpf, nome, telefone, endereco, data_cadastro) VALUES 
 (1, '11111111101', 'Lucas de Almeida Souza', '11988881001', 'Av. Getúlio Vargas, 1500', '2025-11-10'),
 (2, '11111111102', 'Mariana Castilho', '11988881002', 'Rua São Domingos, 45', '2025-11-15'),
 (3, '11111111103', 'Pedro Henrique Nogueira', '11988881003', 'Av. Maria Quitéria, 880', '2025-12-02'),
@@ -80,7 +64,35 @@ INSERT INTO clientes (id_cliente, cpf, nome_completo, telefone, endereco, data_c
 (29, '11111111129', 'Larissa Freitas', '11988881029', 'Rua do Bosque, 73', '2026-03-08'),
 (30, '11111111130', 'Vinicius Moraes Neto', '11988881030', 'Loteamento Nova Esperança', '2026-03-10');
 
-INSERT INTO produtos (id_produto, nome, valor_unitario, estoque, id_categoria) VALUES 
+-- Entregadores
+INSERT INTO entregadores (id, nome, telefone, placa_veiculo) VALUES 
+(1, 'Tiago Silva', '11977771111', 'RTL-8A99'), (2, 'Roberto Santos', '11977772222', 'QKW-2B34'),
+(3, 'Fernando Costa', '11977773333', 'PLM-9C12');
+
+-- Fornecedores
+INSERT INTO fornecedores (id, cnpj, nome_fantasia, telefone, email) VALUES 
+(1, '12345678000199', 'Boi Nobre', '1130001010', 'vendas@boinobre.com.br'), 
+(2, '98765432000188', 'Hortifruti Raiz', '1130002020', 'pedidos@hortiraiz.com.br'), 
+(3, '45612378000177', 'Gela Guela', '1130003030', 'contato@gelaguela.com.br');
+
+--Ingredientes
+INSERT INTO ingredientes (id, nome, unidade_medida, estoque_atual) VALUES 
+(1, 'Pão Brioche', 'Unid', 150), (2, 'Blend Bovino', 'Unid', 200), (3, 'Queijo Cheddar', 'Fatia', 400), 
+(4, 'Alface', 'Maço', 15), (5, 'Tomate', 'Kg', 12), (6, 'Bacon', 'Kg', 18), 
+(7, 'Batata', 'Kg', 80), (8, 'Maionese Verde', 'L', 5);
+
+--Mesas
+INSERT INTO mesas (id, capacidade, status) VALUES 
+(1, 2, 'Livre'), (2, 4, 'Ocupada'), (3, 4, 'Livre'), (4, 6, 'Reservada'), (5, 2, 'Livre');
+
+-- Promocoes
+INSERT INTO promocoes (id, nome, desconto, data_inicio, data_fim) VALUES 
+(1, 'Terça do Smash', 50.00, '2026-03-01', '2026-12-31'), 
+(2, 'Combo Galera', 15.00, '2026-03-06', '2026-03-06'), 
+(3, 'Mês de Aniversário', 10.00, '2026-03-01', '2026-03-31');
+
+-- Produtos
+INSERT INTO produtos (id, nome, valor_unitario, estoque, id_categoria) VALUES 
 (1, 'Smash Clássico', 22.90, 50, 1), (2, 'Smash Salad', 24.90, 50, 1), (3, 'Smash Duplo Bacon', 32.90, 40, 1), 
 (4, 'Smash Triplo Monstro', 39.90, 30, 1), (5, 'Burger Costela BBQ', 36.90, 30, 2), (6, 'Chicken Burger', 29.90, 20, 2), 
 (7, 'Futuro Veggie', 38.00, 15, 2), (8, 'Gorgonzola Premium', 37.90, 40, 2), (9, 'X-Tudo Raiz', 34.90, 35, 2), 
@@ -92,7 +104,12 @@ INSERT INTO produtos (id_produto, nome, valor_unitario, estoque, id_categoria) V
 (25, 'Dadinhos Tapioca', 26.90, 40, 3), (26, 'Combo Smash Clássico', 38.90, 20, 1), (27, 'Combo Costela BBQ', 52.90, 25, 2), 
 (28, 'Combo Casal', 89.90, 20, 1), (29, 'Combo Kids', 29.90, 10, 1), (30, 'Adicional Maionese', 3.50, 150, 3);
 
-INSERT INTO pedidos (id_pedido, data, status, id_funcionario, id_cliente, id_mesa) VALUES 
+-- Compras_estoque
+INSERT INTO compras_estoque (id, id_fornecedor, data, valor_total) VALUES 
+(1, 1, '2026-03-01', 2500.00), (2, 2, '2026-03-05', 450.00), (3, 3, '2026-03-10', 1200.00);
+
+-- Pedidos
+INSERT INTO pedidos (id, data, status, id_funcionario, id_cliente, id_mesa) VALUES 
 (1, '2026-03-08', 'Finalizado', 3, 1, 1), (2, '2026-03-08', 'Finalizado', 1, 2, 2),
 (3, '2026-03-08', 'Finalizado', 3, 3, NULL), (4, '2026-03-09', 'Finalizado', 1, 4, 3),
 (5, '2026-03-09', 'Finalizado', 3, 5, NULL), (6, '2026-03-09', 'Finalizado', 1, 6, 4),
@@ -109,30 +126,37 @@ INSERT INTO pedidos (id_pedido, data, status, id_funcionario, id_cliente, id_mes
 (27, '2026-03-11', 'Novo', 3, 27, NULL), (28, '2026-03-11', 'Aguardando', 1, 28, 2),
 (29, '2026-03-11', 'Novo', 3, 29, 3), (30, '2026-03-11', 'Novo', 1, 30, NULL);
 
-INSERT INTO item_produto (id_item, id_pedido, id_produto, quantidade, valor) VALUES 
-(1,1,1,1,22.90), (2,2,3,2,65.80), (3,3,26,1,38.90), (4,4,10,1,35.90), (5,5,11,2,13.00), (6,6,22,1,34.90), 
-(7,7,5,1,36.90), (8,8,28,1,89.90), (9,9,17,1,22.00), (10,10,8,2,75.80), (11,11,23,1,29.90), (12,12,7,1,38.00), 
-(13,13,27,1,52.90), (14,14,14,3,13.50), (15,15,10,1,35.90), (16,16,4,1,39.90), (17,17,20,2,49.80), (18,18,2,3,74.70), 
-(19,19,12,1,6.50), (20,20,24,1,24.00), (21,21,26,2,77.80), (22,22,6,1,29.90), (23,23,19,1,14.00), (24,24,29,1,29.90), 
-(25,25,16,4,52.00), (26,26,2,1,24.90), (27,27,4,2,79.80), (28,28,21,1,22.90), (29,29,25,1,26.90), (30,30,30,3,10.50);
-
-INSERT INTO receitas (id_receita, id_produto, id_ingrediente, quantidade_necessaria, modo_de_preparo) VALUES 
-(1, 3, 1, 1.00, 'Tostar pão'), (2, 3, 2, 2.00, 'Fazer smash'), (3, 3, 3, 2.00, 'Derreter cheddar');
-
-INSERT INTO compras_estoque (id_compra, id_fornecedor, data_compra, valor_total) VALUES 
-(1, 1, '2026-03-01', 2500.00), (2, 2, '2026-03-05', 450.00), (3, 3, '2026-03-10', 1200.00);
-
-INSERT INTO itens_compra (id_item_compra, id_compra, id_ingrediente, quantidade, preco_unitario) VALUES 
-(1, 1, 2, 100.00, 25.00), (2, 2, 4, 20.00, 4.50), (3, 3, 8, 50.00, 18.00);
-
-INSERT INTO escala_trabalho (id_escala, id_funcionario, id_turno, data_escala) VALUES 
+-- Escala_trabalho
+INSERT INTO escala_trabalho (id, id_funcionario, id_turno, data_escala) VALUES 
 (1, 1, 1, '2026-03-11'), (2, 2, 2, '2026-03-11'), (3, 3, 2, '2026-03-11');
 
-INSERT INTO entregas (id_entrega, id_pedido, id_entregador, endereco_destino, taxa_entrega, status_entrega) VALUES 
-(1, 3, 1, 'Av. Maria Quitéria, 880', 7.00, 'Entregue'), (2, 5, 2, 'Av. João Durval, 3400', 8.50, 'Entregue'), (3, 8, 3, 'Trav. Bandeirantes, 12', 6.00, 'Entregue');
+-- Receitas
+INSERT INTO receitas (id, id_produto, id_ingrediente, quantidade_necessaria, preparo) VALUES 
+(1, 3, 1, 1.00, 'Tostar pão'), (2, 3, 2, 2.00, 'Fazer smash'), (3, 3, 3, 2.00, 'Derreter cheddar');
 
-INSERT INTO pagamentos (id_pagamento_realizado, id_pedido, id_pagamento, valor_pago, data_pagamento) VALUES 
+--itens_compra
+INSERT INTO itens_compra (id, id_compra, id_ingrediente, quantidade, valor_unitario, valor_total) VALUES 
+(1, 1, 2, 100.00, 25.00, 2500.00), (2, 2, 4, 100.00, 4.50, 450.00), (3, 3, 8, 66.67, 18.00, 1200.00);
+
+-- Item_produto
+INSERT INTO item_produto (id, id_pedido, id_produto, quantidade, valor_unitario, valor_total) VALUES 
+(1,1,1,1,22.90, 22.90), (2,2,3,2,32.90, 65.80), (3,3,26,1,38.90, 38.90), (4,4,10,1,35.90, 35.90), (5,5,11,2,6.50, 13.00), (6,6,22,1,34.90, 34.90), 
+(7,7,5,1,36.90, 36.90), (8,8,28,1,89.90, 89.90), (9,9,17,1,22.00, 22.00), (10,10,8,2,37.90, 75.80), (11,11,23,1,29.90, 29.90), (12,12,7,1,38.00, 38.00), 
+(13,13,27,1,52.90, 52.90), (14,14,14,3,4.50, 13.50), (15,15,10,1,35.90, 35.90), (16,16,4,1,39.90, 39.90), (17,17,20,2,24.90, 49.80), (18,18,2,3,24.90, 74.70), 
+(19,19,12,1,6.50, 6.50), (20,20,24,1,24.00, 24.00), (21,21,26,2,38.90, 77.80), (22,22,6,1,29.90, 29.90), (23,23,19,1,14.00, 14.00), (24,24,29,1,29.90, 29.90), 
+(25,25,16,4,13.00, 52.00), (26,26,2,1,24.90, 24.90), (27,27,4,2,39.90, 79.80), (28,28,21,1,22.90, 22.90), (29,29,25,1,26.90, 26.90), (30,30,30,3,3.50, 10.50);
+
+-- Pagamentos
+INSERT INTO pagamentos (id, id_pedido, id_metodo, valor_pago, data_pagamento) VALUES 
 (1, 1, 1, 22.90, '2026-03-08'), (2, 2, 2, 65.80, '2026-03-08'), (3, 3, 1, 38.90, '2026-03-08');
 
-INSERT INTO avaliacoes (id_avaliacao, id_cliente, id_pedido, nota, comentario) VALUES 
+-- Entregas
+INSERT INTO entregas (id, id_pedido, id_entregador, endereco, taxa, status) VALUES 
+(1, 3, 1, 'Av. Maria Quitéria, 880', 7.00, 'Entregue'), (2, 5, 2, 'Av. João Durval, 3400', 8.50, 'Entregue'), (3, 8, 3, 'Trav. Bandeirantes, 12', 6.00, 'Entregue');
+
+-- Avaliações
+INSERT INTO avaliacoes (id, id_cliente, id_pedido, nota, comentario) VALUES 
 (1, 1, 1, 5, 'Melhor smash!'), (2, 2, 2, 4, 'Muito bom!'), (3, 3, 3, 5, 'Maionese viciante!');
+
+
+
